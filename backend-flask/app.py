9dd1b3772a273eb09f4f25e6e9e6d3ebda254817
ucaninthecloud <alfonso@ucaninthe.cloud>
2023-03-06 05:48:50 -0600

    Instrument Honeycomb

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -29,9 +29,14 @@ provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
-
-
 app = Flask(__name__)
+
+# HoneyComb
+# Initialize automatic instrumentation with Flask
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
+
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
