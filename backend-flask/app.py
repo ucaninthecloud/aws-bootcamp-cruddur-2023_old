9902d3a265e7299fb147c124c60dd514a99b6f43
ucaninthecloud <alfonso@ucaninthe.cloud>
2023-03-07 21:58:44 -0600

    Added XRay config and sampling

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -13,6 +13,10 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+# X-Ray 
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
 # HoneyComb
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
@@ -28,6 +32,11 @@ processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
+
+# XRay recorder 
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
 
 app = Flask(__name__)
 
