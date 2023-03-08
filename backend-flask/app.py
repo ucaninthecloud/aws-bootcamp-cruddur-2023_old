853bce07a8796a8daba8fb228f93be6b9e21a6c5
ucaninthecloud <alfonso@ucaninthe.cloud>
2023-03-07 22:27:58 -0600

    Fixed AWS XRay location

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -36,9 +36,11 @@ tracer = trace.get_tracer(__name__)
 # XRay recorder 
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
+
 
 app = Flask(__name__)
+
+XRayMiddleware(app, xray_recorder)
 
 # HoneyComb
 # Initialize automatic instrumentation with Flask
